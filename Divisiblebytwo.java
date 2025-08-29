24119c5b93f02c36383bb187e198a5f605de3695
LOGESH <logeshshanmugam678@gmail.com>
2025-08-29 19:49:54 +0530

    Add files via upload

---
+++ Divisiblebytwo.java
@@ -0,0 +1,20 @@
+package javapractise;
+
+import java.util.Scanner;
+
+public class Divisiblebytwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num=scan.nextInt();
+		
+		String result=(num%2==0)?"Divisible by 2":"Not Divisible by 2";
+		
+		System.out.println(result);
+	}
+
+}
+//Write a program to find a number which is divisible by 2 using only ternary operator
